62ff43fd5cdfe5b06b16f9557abcf4fd22e510b3
cogas <cheeky.chic.chick@gmail.com>
2014-07-20 00:39:41 +0900

    rev

--- sidebar.js
+++
@@ -1,5 +0,0 @@
-var link = document.querySelector('link[rel="import"]');
-var content = link.import;
-var el = content.querySelector('.sidebar');
-
-document.getElementById("sideWrap").appendChild(el.cloneNode(true));
