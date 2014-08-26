d71b40e90469d7f1041f729ff7f67685b0e26178
cogas <cheeky.chic.chick@gmail.com>
2014-08-26 22:22:13 +0900

    yesnoquestion実�

--- karni/vocab.js
+++ karni/vocab.js
@@ -11,4 +11,25 @@ $(window).load(function () {
 				.append($("<td class='meaning'></td>").text(dd[i]))
 		);
 	}
+
+
 });
+
+function yesnocheck(){
+	var anstemp = $("input[name='answer']").val();
+	var num = anstemp.length;
+	var answer = [];
+	for (var i=0; i<num; i++){
+		answer.push(parseInt(anstemp[i]));
+	}
+	var data = [];
+	for (var i=0; i<num; i++){
+		data.push(parseInt($("input[name='q"+i+"']:checked").val()));
+	}
+	var cor = 0;
+	for (var i=0; i<num; i++){
+		if (data[i] == answer[i]) cor++;
+	}
+	$("#yesnoans").empty().append(cor+"/"+num+"問正解！");
+	if(cor==num) $("#yesnoans").append("全問正解！おめでとう！");
+}
