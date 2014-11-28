c9a73f296993a5bcaba448945073aea71aedc23e
cogas <cheeky.chic.chick@gmail.com>
2014-11-28 18:55:25 +0900

    印刷ボタン

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -14,3 +14,37 @@ $(window).load(function () {
 　$('.target_blank a').attr("target='_blank'");
 });
 
+$(function() {
+
+    //プリントプレビュークリックイベント
+    $(document).on('click','.print-preview',function(){
+        doPrintPreview();
+    });
+
+});
+
+/**
+ * プリントプレビュー
+ */
+doPrintPreview = (function(){
+    //現在のURLに'print'パラメータを付加し、新しいウィンドウで表示させる。
+    window.open(location.href+'?print=true');
+});
+
+
+$(function() {
+    //URLパラメータに'print'が存在する場合、
+    if (location.href.indexOf('?print') > -1) { 
+        //bodyにprintクラスを付加。
+        $("body").addClass('print');
+
+        //.btnBoxの中身を空にし、印刷ボタンをアペンド。
+        $('.btnBox').html('').append('<input type="button" value="印刷する" id="doPrint" name="doPrint">');
+
+        //印刷ボタンのクリックイベントに印刷機能を設定
+        $('#doPrint').on('click', function(){
+            window.print();
+        });
+
+    }
+});
