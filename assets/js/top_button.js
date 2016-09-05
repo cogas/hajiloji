17053eec02f89026c1c7437ac75d587c08b3497c
cogas <cheeky.chic.chick@gmail.com>
2016-09-05 19:44:37 +0900

    レスポンシブデザイン＆細々
    viewport、上に戻るボタン、表のデザイン変更、上の無意味な余白の削除

---
+++ assets/js/top_button.js
@@ -0,0 +1,19 @@
+$(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが500に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
